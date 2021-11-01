0e47e065f9b395a82d7fcb49a1d3cf67685e93cc
rheophile10 <rheophile10@gmail.com>
2021-11-01 13:53:59 -0600

    mkdirs raw_data and finished data

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fda_data_getter.purple_book import PurpleBook
 from fda_data_getter.ndc import NDCBook
 from time import time
 import sys
+import os
 
 def main(format='xlsx'):
     print('\033[94mGetting fda and NDC data\033[0m')
@@ -18,6 +19,9 @@ def main(format='xlsx'):
     print(f'\033[92mDone! Operation took {elapsed} minutes\033[0m')
 
 if __name__ == '__main__': 
+    for dirs in ['raw_data', 'finished data']:
+        if not os.path.exists(dirs):
+            os.mkdirs(dirs)
     if len(sys.argv) > 1: 
         run_format = sys.argv[1]
         if run_format not in ('xlsx', 'csv'):
